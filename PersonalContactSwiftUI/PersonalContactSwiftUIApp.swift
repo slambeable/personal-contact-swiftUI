3a633379dee9c27e84506c76f8c249a90b29317c
slambeable <41022916+slambeable@users.noreply.github.com>
2022-03-22 22:10:45 +0300

    Initial Commit

---
+++ PersonalContactSwiftUI/PersonalContactSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalContactSwiftUIApp.swift
+//  PersonalContactSwiftUI
+//
+//  Created by Андрей Евдокимов on 22.03.2022.
+//
+
+import SwiftUI
+
+@main
+struct PersonalContactSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
